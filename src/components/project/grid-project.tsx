f92b9f8d4246d5d0eb545ec1956428e3034aa530
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-09-30 15:09:36 +0200

    key id added

--- src/components/project/grid-project.tsx
+++ src/components/project/grid-project.tsx
@@ -9,7 +9,7 @@ export function GridProject() {
       <div className="grid sm:grid-cols-1 md:grid-cols-2 h-full">
         {ProjectsData.map((project) => {
           return (
-            <Card>
+            <Card key={project.id}>
               <h1 className="text-4xl text-indigo-700">{project.title}</h1>
               <div className="flex flex-row gap-4 h-max">
                 <Button kind="default">View Demo</Button>
