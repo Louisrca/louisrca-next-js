b7b28240ba102c2b6ea0fc1b8203a3a32aed0a19
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-10-01 23:43:56 +0200

    grid card small screen

--- src/components/footer/footer-section.tsx
+++ src/components/footer/footer-section.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <section className="flex  justify-center items-center text-indigo-500 ">
+    <section className="sm:h-screen flex justify-center items-center text-indigo-500 ">
       Louis Rocca 2023{" "}
     </section>
   );
