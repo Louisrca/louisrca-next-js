531738aedb5d5c4a9dc6b6ac73468581bbdda03d
Louis <louisrca@MacBook-Pro-de-Louis.local>
2023-10-03 23:35:00 +0200

    padding & margin title and button landing page

--- src/components/ui/h1.tsx
+++ src/components/ui/h1.tsx
@@ -5,13 +5,14 @@ export const H1 = ({
   children,
   kind = "default",
 }: PropsWithChildren & {
-  kind?: "default" | "indigo" | "green" | "white";
+  kind?: "default" | "indigo" | "green" | "white" | "white-mobile";
 }) => {
   return (
     <h1
       className={clsx("text-6xl text-green-400 font-bold", {
         "text-6xl text-indigo-700 font-bold": kind === "indigo",
         "text-6xl text-white font-normal": kind === "white",
+        "text-5xl text-white font-normal": kind === "white-mobile",
       })}
     >
       {children}
